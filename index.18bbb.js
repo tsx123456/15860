dee088aed2ed8aefad59b66e221f715ef2f24114
上帝的梦 <1597691532@qq.com>
2026-04-16 17:59:00 +0800

    1

--- index.18bbb.js
+++ index.18bbb.js
@@ -1,4 +1,4 @@
-System.register(["./application.c5e68.js"], function (_export, _context) {
+System.register(["./application.f200a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
